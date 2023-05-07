3fbbc4eff9921b59f51f0edafdf7460a4e077009
kamai <kamai1220000@gmail.com>
2023-05-07 12:55:54 +0300

    feat: added test e2e , unit , integration

---
+++ src/entities/layout/ui/footer/footer.test.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+import { Footer } from './index'
+
+describe('Тестирование Footer', () => {
+	it('on document', () => {
+		const footer = render(<Footer />)
+
+		expect(footer.getByText('With Feature-Sliced Design')).toBeInTheDocument()
+	})
+})
---
+++ src/entities/layout/ui/sider-btn/indexSiderBtn.cy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SiderBtn } from './index'
+
+describe('<SiderBtn />', () => {
+	it('renders', () => {
+		// see: https://on.cypress.io/mounting-react
+		cy.mount(<SiderBtn />)
+	})
+})
