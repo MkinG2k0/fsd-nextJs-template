b218afdb5a13dbbc62774d895b970e7be48eb47f
kamai <kamai1220000@gmail.com>
2023-06-12 20:31:50 +0300

    fix:

--- src/shared/templates/comp/index.tsx
+++
@@ -1,7 +0,0 @@
-import style from './style.module.scss'
-
-interface FileNameProps {}
-
-export const FileName: FC<FileNameProps> = ({}) => {
-	return <div className={style.wrap}>FileName</div>
-}
--- src/shared/templates/page/ui/page/index.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-import style from './FileName.module.scss'
-
-interface FileNameProps {}
-
-export const Todo: FC<FileNameProps> = () => {
-	return <div className={style.wrap}>FileName</div>
-}
